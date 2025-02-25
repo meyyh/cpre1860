f1104108872289fc0c12934604b5afaf13184781
meyyh <cooperrs123@gmail.com>
2025-02-25 12:02:12 -0600

    asdf

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import com.github.bhlangonijr.chesslib.Board;
 
 
 public class Main {
-    public static void main(String[] args) {  // Fixed the signature
+    public static void main(String[] args) {
         Board board = new Board();
         board.doMove(new Move(Square.E2, Square.E4));
         System.out.println(board.toString());
