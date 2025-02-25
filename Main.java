84933273dd53705154c818163c6a78d84f668fe5
meyyh <cooperrs123@gmail.com>
2025-02-25 10:59:24 -0600

    using source now

---
+++ Main.java
@@ -0,0 +1,12 @@
+import com.github.bhlangonijr.chesslib.*;
+import com.github.bhlangonijr.chesslib.move.Move;
+import com.github.bhlangonijr.chesslib.Board;
+
+
+public class Main {
+    public static void main(String[] args) {  // Fixed the signature
+        Board board = new Board();
+        board.doMove(new Move(Square.E2, Square.E4));
+        System.out.println(board.toString());
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://www.instructables.com/DIY-Super-Smart-Chessboard-Play-Online-or-Against-
   
 Hall Effect Sensors
 https://www.nutsvolts.com/magazine/article/January2017_Electronic-Chessboard-LEDs-Hall-Effect-Sensors
+
+to run the code do ```javac -cp target/classes Main.java``` then ```java -cp .:target/classes Main```
