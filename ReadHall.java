8c0f802856a34d435593590e3159afad084d5ab4
meyyh <cooperrs123@gmail.com>
2025-03-04 13:53:28 -0600

    asdf

---
+++ ReadHall.java
@@ -0,0 +1,23 @@
+
+public class ReadHall {
+    public static String pulse_sensors()
+    {
+        int nochange = 0;
+        while(nochange == 0)
+        {
+            //send power via gpio class
+            //get back
+            int[] ret = handle_gpio();
+        }
+        return "asdf";
+    }
+
+    private static  int[] handle_gpio()
+    {
+        int[] row_data = new int[9];
+        int[][] boardval
+        return row_data;
+
+
+    }
+}
